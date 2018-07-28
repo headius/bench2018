125c2e58f7b3d303e0d51b11a3ade6c53b84a854
Charles Oliver Nutter <headius@headius.com>
2018-07-28 14:01:50 +0000

    Add run_all script.

--- run_all.rb
+++ run_all.rb
@@ -1,25 +1,28 @@
 scripts = Dir['bench*.rb']
 opts = [
   "",
-  "-Xcompile.invokedynamic",
-  "-Xcompile.invokedynamic -Xfixnum.cache=false",
+  #"-Xcompile.invokedynamic",
+  #"-Xcompile.invokedynamic -Xfixnum.cache=false",
 ]
 gcs = [
-  "-J-XX:+UseG1GC",
-  "-J-XX:+UseParallelGC",
+  "",
+  #"-J-XX:+UseG1GC",
+  #"-J-XX:+UseParallelGC",
 ]
 jits = [
   "",
-  "-J-XX:+UnlockExperimentalVMOptions -J-XX:+EnableJVMCI -J-XX:+UseJVMCICompiler",
+  "--jit",
+  #"-J-XX:+UnlockExperimentalVMOptions -J-XX:+EnableJVMCI -J-XX:+UseJVMCICompiler",
 ]
 java_homes = [
   "/usr/lib/jvm/java-8-openjdk-amd64",
-  "/home/ubuntu/jdk-10.0.1"
+  #"/home/ubuntu/jdk-10.0.1"
 ]
 rubies = [
-  "jruby-9.1.17.0",
-  "jruby-9.2.0.0",
-  "ruby-2.5"
+#  "jruby-9.1.17.0",
+#  "jruby-9.2.0.0",
+  "ruby-2.5",
+  "ruby-head",
 ]
 
 rubies.each do |ruby|
